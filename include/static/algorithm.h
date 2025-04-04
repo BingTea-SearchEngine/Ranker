483556f05b50dbc15aa7b5e34afef5c5ec9f6c5c
prcysprk <prcysprk@umich.edu>
2025-04-04 03:02:13 -0400

    Add RNG

--- include/static/algorithm.h
+++ include/static/algorithm.h
@@ -51,4 +51,41 @@ void quicksort(T* values, int left, int right) {
         quicksort(values, i, right);
 }
 
+class RNG {
+    private:
+    // Internal state (seed)
+    long seed;
+    static const long a = 16807;
+    static const long m = 2147483647; // 2^31 - 1
+
+public:
+    // Constructor with a default seed (must be non-zero)
+    RNG(long init_seed = 1) {
+        if (init_seed == 0)
+            init_seed = 1;
+        seed = init_seed;
+    }
+
+    // Generates the next random integer
+    long gen_int() {
+        seed = (a * seed) % m;
+        return seed;
+    }
+
+    // Generates a random float in the range [0, 1)
+    double gen_double() {
+        return static_cast<double>(gen_int()) / m;
+    }
+
+    template <typename T>
+    void shuffle(T* array, unsigned n) {
+        for (unsigned i = 0; i < n - 1; ++i) {
+            unsigned j = gen_int() % n;
+            T temp = array[i]; // swap semantics are better but whatever
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+};
+
 #endif
