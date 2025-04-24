38f006eafb44b2985bc437b4016ab3acbca823f5
Wonbin Jin <wbjin@umich.edu>
2025-04-24 13:08:30 -0400

    Add louvain

--- src/insertrank.cpp
+++ src/insertrank.cpp
@@ -19,7 +19,9 @@ int main(int argc, char**argv) {
     std::string htmlDirPath = std::string(argv[1]);
     std::string pageRankFilePath = std::string(argv[2]);
     std::string cheiRankFilePath = std::string(argv[3]);
-    std::string hashFilePath = std::string(argv[4]);
+    std::string louvainCommunityPath = std::string(argv[4]);
+    std::string louvainCommunityCountPath = std::string(argv[5]);
+    std::string hashFilePath = std::string(argv[6]);
 
     URLHash hash(hashFilePath, true);
 
@@ -33,7 +35,7 @@ int main(int argc, char**argv) {
         pageranks.push_back(rank);
     }
     }
-    cout << pageranks.size() << endl;
+    cout << "Page rank in memory" << endl;
 
     std::vector<float> cheiranks;
     cheiranks.reserve(37941676);
@@ -45,17 +47,45 @@ int main(int argc, char**argv) {
         cheiranks.push_back(rank);
     }
     }
-    cout << cheiranks.size() << endl;
-    //
+    cout << "Chei rank in memory" << endl;
+
+    std::vector<int> communities;
+    communities.reserve(37941676);
+    {
+    std::string line;
+    std::ifstream communitiesFile(louvainCommunityPath);
+    while(std::getline(communitiesFile, line)) {
+        int community = std::stoi(line);
+        communities.push_back(community);
+    }
+    }
+    cout << "Communities in memory" << endl;
+
+    std::vector<int> communityCounts;
+    communityCounts.reserve(37941676);
+    {
+    std::string line;
+    std::ifstream communitiesCountFile(louvainCommunityCountPath);
+    while(std::getline(communitiesCountFile, line)) {
+        int count = std::stoi(line);
+        communityCounts.push_back(count);
+    }
+    }
+    cout << "Communty counts in memory" << endl;
 
     int count = 0;
     for (const auto& entry : std::filesystem::directory_iterator(htmlDirPath)) {
         if (!entry.is_regular_file()) continue;
-        std::fstream f(entry.path(), std::ios::in | std::ios::out);
-        std::string line;
-        std::getline(f, line);
 
+        std::ifstream f(entry.path(), std::ios::in);
+        std::string content((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
+        f.close();
+
+        std::istringstream stream(content);
         std::regex url_doc_regex(R"(URL:\s*(\S+)\s+Doc number:\s*(\d+))");
+        std::string line;
+        std::getline(stream, line);
+
         std::smatch match;
         std::string url;
         if (std::regex_search(line, match, url_doc_regex)) {
@@ -63,21 +93,34 @@ int main(int argc, char**argv) {
         } else {
             continue;
         }
-        cout << url << endl;
-        unsigned row = hash[url];
-        cout << "Page rank " << pageranks[row] << endl;
-        cout << "Chei rank " << cheiranks[row] << endl;
-        f.clear();
-        f.seekp(0, std::ios::end);
-        f << "<prank>\n";
-        f << pageranks[row] << "\n";
-        f << "</prank>\n";
-        f << "<crank>\n";
-        f << cheiranks[row] << "\n";
-        f << "</crank>\n";
-        f.close();
 
-        cout << count << endl;
+        unsigned row = hash[url];
+        cout << "Url: " << url << "Row: " << row << endl;
+        bool hasPrank = content.find("<prank>") != std::string::npos;
+        bool hasCrank = content.find("<crank>") != std::string::npos;
+        bool hasCommunity = content.find("<community>") != std::string::npos;
+
+        std::ostringstream oss;
+        oss << std::scientific << std::setprecision(6);
+        if (!hasPrank) {
+            oss << "<prank>\n" << pageranks[row] << "\n</prank>\n";
+        }
+        if (!hasCrank) {
+            oss << "<crank>\n" << cheiranks[row] << "\n</crank>\n";
+        }
+        if (!hasCommunity) {
+            oss <<  "<community>\n" << communities[row] << "\n</community>\n";
+            oss <<  "<communitycount>\n" << communityCounts[row] << "\n</communitycount>\n";
+        }
+
+        std::string toAppend = oss.str();
+        if (!toAppend.empty()) {
+            std::ofstream out(entry.path(), std::ios::app);
+            out << toAppend;
+            out.close();
+        }
+
+        cout << count << "\n";
         count++;
     }
     return 0;
